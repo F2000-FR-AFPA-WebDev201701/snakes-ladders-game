a3de46462b1abb6a247164a77d30a4531bb61762
tony <tony.cv@gmail.com>
2017-03-21 17:24:26 +0100

    negligeable

--- src/AppBundle/Model/Board.php
+++ src/AppBundle/Model/Board.php
@@ -31,13 +31,14 @@ class Board {
 
 
 // init. du plateau
+
         for ($i = 0; $i <= 63; $i++) {  // création des 64 cases objets
             $iNumCell = $this->getNumCell($i);  // $this->corr..[$i] // Verif fonction de Pierre
 
             $oCell = new Cell();
             $oCell->setNum($iNumCell);
 
-            // Attribution difficultés des cases
+// Attribution difficultés des cases
             If (($i == 0 || $i == 4 || $i == 9 || $i == 11 || $i == 14 || $i == 16 || $i == 26 || $i == 29 || $i == 31 || $i == 17) || $i == 55 || ($i > 34 && $i < 40) || ($i > 43 && $i < 47) || ($i > 53 && $i < 56)) {
                 $oCell->setLevel(Cell::LEVEL_MEDIUM);
             } else if (($i == 42 || $i == 43 || $i == 34 || $i == 32 || $i == 23 || $i == 20 ) || ($i > 56 && $i < 65) || ($i > 46 && $i < 54) || ($i > 25 && $i < 28)) {
@@ -51,7 +52,7 @@ class Board {
     }
 
     public function initPlayer($aoUsers) {
-        // init. des joueurs
+// init. des joueurs
         foreach ($aoUsers as $oUser) {  // pour chaque utilisateur (qui joue?) creer un nouveau Pawn avec une position à 0
             $oPawn = new Pawn($oUser);
             $oPawn->setPosition(0); // numéro de la case twig
@@ -108,11 +109,11 @@ class Board {
 // This function take as input the oActualPawn and will return :
 // the new position value of the pawn (in twig case) and change the tab cells with the new position of the pawn inside
     public function movePawn($oPawn) {
-        // Remove into the cell the place where the pawn were
+// Remove into the cell the place where the pawn were
         $iOldCell = $this->getIdxCell($oPawn->getPosition());
         $this->cells[$iOldCell]->removePawn($oPawn);
 
-        // Calcul of the new position
+// Calcul of the new position
         $iLastPosition = $oPawn->getPosition();
         $iDiceValue = $this->getDice();
         $iNewPosition = $iLastPosition + $iDiceValue;
@@ -120,7 +121,7 @@ class Board {
             $iNewPosition = 63;
         }
         $this->pawns[$this->playerTurn]->setPosition($iNewPosition);
-        // Add into the new cell the pawn
+// Add into the new cell the pawn
         $iNewCell = $this->getIdxCell($this->pawns[$this->playerTurn]->getPosition());
         $this->cells[$iNewCell]->addPawn($this->pawns[$this->playerTurn]);
         return;
@@ -195,13 +196,13 @@ class Board {
 
         if (count($this->cells[self::getIdxCell(63)]->getPawns()) != 0) {
             $bEndOfGame = 1; // si un pion arrive dans la case 63 on retourne un booléen = 1 pour dire que la partie est finie
-            // attribut "statut" de l'objet oGame = KO => fair dans le GameControleur
+// attribut "statut" de l'objet oGame = KO => fair dans le GameControleur
         }
         return $bEndOfGame;
     }
 
     function malusPawn($oPawn, $level) {
-        // Level = niveau de difficulté de la case/question
+// Level = niveau de difficulté de la case/question
         $comment = '';
         $nbCaseMalus = 0;
 
@@ -219,18 +220,18 @@ class Board {
                 $nbCaseMalus = 1;
                 break;
         }
-        // On enlève le pion de la case actuelle
+// On enlève le pion de la case actuelle
         $iOldCell = $this->getIdxCell($oPawn->getPosition());
         $this->cells[$iOldCell]->removePawn($oPawn);
 
-        // calcul de la nouvelle position
+// calcul de la nouvelle position
         if (($oPawn->getPosition()) < $nbCaseMalus) {
             $oPawn->setPosition(0);
         } else {
             $oPawn->setPosition(($oPawn->getPosition()) - $nbCaseMalus);
         }
 
-        // on ajoute le pion sur la nouvelle case
+// on ajoute le pion sur la nouvelle case
         $iNewCell = $this->getIdxCell($oPawn->getPosition());
         $this->cells[$iNewCell]->addPawn($oPawn);
 
@@ -238,7 +239,7 @@ class Board {
     }
 
     function bonusPawn($oPawn, $level) {
-        // Level = niveau de difficulté de la case/question
+// Level = niveau de difficulté de la case/question
         $comment = '';
         $nbCaseBonus = 0;
 
@@ -256,18 +257,18 @@ class Board {
                 $nbCaseBonus = 4;
                 break;
         }
-        // On enlève le pion de la case actuelle
+// On enlève le pion de la case actuelle
         $iOldCell = $this->getIdxCell($oPawn->getPosition());
         $this->cells[$iOldCell]->removePawn($oPawn);
 
-        // calcul de la nouvelle position
+// calcul de la nouvelle position
         if ((($oPawn->getPosition()) + $nbCaseBonus) > 63) {
             $oPawn->setPosition(63);
         } else {
             $oPawn->setPosition(($oPawn->getPosition()) + $nbCaseBonus);
         }
 
-        // on ajoute le pion sur la nouvelle case
+// on ajoute le pion sur la nouvelle case
         $iNewCell = $this->getIdxCell($oPawn->getPosition());
         $this->cells[$iNewCell]->addPawn($oPawn);
 
